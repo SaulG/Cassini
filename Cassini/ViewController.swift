0d0db1e65e09e51050152bdb61796e225fa4e8db
Saul Gausin <saul.gausin@gmail.com>
2015-07-02 13:18:54 -0500

    Initial commit

--- Cassini/ViewController.swift
+++ Cassini/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func fetchImage()
+    {
+        if let url = imageURL {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil {
+                image = UIImage(data: imageData!)
+            } else {
+                image = nil
+            }
+        }
     }
-
-
+    
+    private var imageView: UIImageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
 }
 
